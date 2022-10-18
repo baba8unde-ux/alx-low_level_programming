1cbd5743ffdee704434a2910471d285bfa3b3e27
Babatunde Lawal <107517723+baba8unde-ux@users.noreply.github.com>
2022-10-19 00:59:16 +0100

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,8 @@
 int _putchar(char c);
 
-/**
- * print_alphabet - print lowercase alphabet
- */
 
 void print_alphabet(void);
 
 
-/**
- * print_alphabet_x10 - print lowercase alphabet ten times
- */
-
 void print_alphabet_x10(void);
 
-/**
- * _islower - checks for lowercase
- * @c: the character to check
- * Return: int
- */
