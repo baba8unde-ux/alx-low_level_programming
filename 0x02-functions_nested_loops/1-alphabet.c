3c7f2152b3351d93e1804e6e91feeac55fcc40ff
Babatunde Lawal <107517723+baba8unde-ux@users.noreply.github.com>
2022-10-18 23:08:50 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints lowercase alphabet
- * Return: 0
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
-void print_alphabet(void)
+int main(void)
 {
-	char alpha;
-
-	alpha = 'a';
-
-	while (alpha <= 'z')
-	{
-		_putchar(alpha);
-		alpha++;
-	}
-	_putchar('\n');
+    print_alphabet();
+    return (0);
 }
-
