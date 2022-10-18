1cc12f97b75f82fb83e60b9adc914220d734551d
Babatunde Lawal <107517723+baba8unde-ux@users.noreply.github.com>
2022-10-18 23:35:20 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
-
 /**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
+ * main - print putchar
+ * Return: always 0
+ */
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('t');
-	_putchar('a');
-	_putchar('r')
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("t");
+	_putchar("a");
+	_putchar("r");
 
 	return (0);
 }
