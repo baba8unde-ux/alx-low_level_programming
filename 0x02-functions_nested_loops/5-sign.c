8f54f64f7d7fffb551b338923b5d8323e222046c
Babatunde Lawal <107517723+baba8unde-ux@users.noreply.github.com>
2022-10-19 02:01:50 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,21 +6,14 @@
  * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
  */
 
-int print_sign(int n)
+int main(void)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
+	int r;
+
+	r = print_sign(98);
+	if (r < 0)
+		r *= -1;
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
 }
