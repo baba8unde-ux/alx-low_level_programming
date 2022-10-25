3ec2ef7cbae35a69cfd8bb6a1ca5572da261b874
Babatunde Lawal <107517723+baba8unde-ux@users.noreply.github.com>
2022-10-25 02:26:58 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,14 @@
 #include "main.h"
-/**
- *_puts - prints a string
- * @str: string to print
- *
- * Description: prints a string
- * On success: return the number of characters printed
- */
 
+/**
+ * _puts - write string followed by new line
+ * @str: string
+ * Return: void
+ */
 void _puts(char *str)
 {
-while (*str)
-_putchar(*str++);
+	while (*str)
+		_putchar(*str++);
 
-_putchar('\n');
+	_putchar('\n');
 }
